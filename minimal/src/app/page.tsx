8116cde57dde81befe6c61a5f1428551c680fb7a
IgnjatMarko <ignjatovicmarko2023@gmail.com>
2025-03-08 21:46:08 +0100

    Added directory for Minimal Shop from KokonutUI

---
+++ minimal/src/app/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import MinimalShop from "@/components/kokonutui/minimal-shop";
+
+export default function Home() {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <MinimalShop />
+    </div>
+  );
+}
